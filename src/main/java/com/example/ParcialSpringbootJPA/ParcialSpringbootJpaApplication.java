59512a38f1e2fc958ef1e1e3b0fec8068b3ce4d3
DeisyMonroy <deisy.monroy@uptc.edu.co>
2024-11-28 18:11:00 -0500

    Estructuta del proyecto

---
+++ src/main/java/com/example/ParcialSpringbootJPA/ParcialSpringbootJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ParcialSpringbootJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialSpringbootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcialSpringbootJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ParcialSpringbootJPA/ParcialSpringbootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ParcialSpringbootJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialSpringbootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
